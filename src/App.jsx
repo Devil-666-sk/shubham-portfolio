9d38fdff3ca9c2b5a4e23ce307ca32fb4c7fa510
Devil-666-sk <sk6930037@gmail.com>
2025-05-19 22:40:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Typography from '@mui/material/Typography'
+
+function App() {
+  
+
+  return (
+    <>
+    <Typography variant="h1" color="initial">Hii Guys This Side Shubham Kumar MERN Stack Developer</Typography>
+    </>
+  )
+}
+
+export default App
